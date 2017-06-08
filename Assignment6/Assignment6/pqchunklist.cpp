2663ed359c8a22ec4bd83cc5144eebac25f11325
antonc27 <antonc27@mail.ru>
2017-06-09 00:22:39 +0200

    - Assignment 6: Chunk list implementation

--- Assignment6/Assignment6/pqchunklist.cpp
+++ Assignment6/Assignment6/pqchunklist.cpp
@@ -9,7 +9,7 @@
 
  */
 
-
+const int MaxElemsPerBlock = 5;
 
 /* Implementation notes: constructor
  * ---------------------------------
@@ -17,17 +17,22 @@
  */
 PQueue::PQueue()
 {
-    
+    head = NULL;
 }
 
 PQueue::~PQueue()
 {
-    
+    while (head != NULL) {
+        Chunk *next = head->next;
+        delete head->values;
+        delete head;
+        head = next;
+    }
 }
 
 bool PQueue::isEmpty()
 {
-    return true;
+    return (head == NULL);
 }
 
 /* Implementation notes: size
@@ -36,7 +41,10 @@ bool PQueue::isEmpty()
  */
 int PQueue::size()
 {
-    return 0;
+    int count = 0;
+    for (Chunk *cur = head; cur != NULL; cur = cur->next)
+        count += cur->numUsed;
+    return count;
 }
 
 
@@ -46,7 +54,41 @@ int PQueue::size()
  */
 void PQueue::enqueue(int newValue)
 {
- 	
+    if (head == NULL) {
+        head = newChunk();
+        insertToChunk(newValue, head);
+        return;
+    }
+    
+    Chunk *previousChunk = NULL;
+    Chunk *chunkToInsert = head;
+    while (true) {
+        if (chunkToInsert == NULL) {
+            //allocate new chunk
+            chunkToInsert = newChunk();
+            if (previousChunk != NULL) {
+                previousChunk->next = chunkToInsert;
+            }
+            // insert to current
+            break;
+        } else if (chunkToInsert->next == NULL) {
+            // insert to current
+            break;
+        } else {
+            int maxCurrent = chunkToInsert->values[0];
+            int maxNext = chunkToInsert->next->values[0];
+            
+            if (newValue > maxCurrent || newValue > maxNext) {
+                // insert to current
+                break;
+            } else {
+                // go to next chunk
+                previousChunk = chunkToInsert;
+                chunkToInsert = chunkToInsert->next;
+            }
+        }
+    }
+    insertToChunk(newValue, chunkToInsert);
 }
 
 
@@ -59,7 +101,18 @@ int PQueue::dequeueMax()
 	if (isEmpty())
 		Error("Tried to dequeue max from an empty pqueue!");
 	
-	return 0;
+    int max = head->values[0];
+    head->numUsed--;
+    for (int i = 0; i < head->numUsed; i++) {
+        head->values[i] = head->values[i+1];
+    }
+    if (head->numUsed == 0) {
+        Chunk *next = head->next;
+        delete head->values;
+        delete head;
+        head = next;
+    }
+	return max;
 }
 
 
@@ -69,7 +122,10 @@ int PQueue::dequeueMax()
  */
 int PQueue::bytesUsed()
 {
-    return 0;
+    int total = sizeof(*this);
+    for (Chunk *cur = head; cur != NULL; cur = cur->next)
+        total += sizeof(*cur) + sizeof(int)*cur->numUsed;
+    return total;
 }
 	
 
@@ -85,8 +141,64 @@ string PQueue::implementationName()
  */
 void PQueue::printDebuggingInfo()
 {
+    int count = 0;
+    
     cout << "------------------ START DEBUG INFO ------------------" << endl;
-
+    for (Chunk *cur = head; cur != NULL; cur = cur->next) {
+        cout << "Chunk #" << count << " (at address " << cur << ") vals = [";
+        for (int i = 0; i<cur->numUsed; i++) {
+            cout << cur->values[i] << ", ";
+        }
+        cout << "] next = " << cur->next << endl;
+        count++;
+    }
     cout << "------------------ END DEBUG INFO ------------------" << endl;
+}
+
+PQueue::Chunk * PQueue::newChunk() {
+    Chunk *chunk = new Chunk();
+    chunk->values = new int[MaxElemsPerBlock];
+    chunk->numUsed = 0;
+    chunk->next = NULL;
+    return chunk;
+}
+
+void PQueue::insertToChunk(int newValue, PQueue::Chunk *&chunkToInsert) {
+    if (chunkToInsert->numUsed == 0) {
+        chunkToInsert->values[0] = newValue;
+        chunkToInsert->numUsed++;
+    } else if (chunkToInsert->numUsed == MaxElemsPerBlock) {
+        // split chunk
+        int halfSize = MaxElemsPerBlock / 2 + 1;
+        Chunk *nextNewChunk = newChunk();
+        for (int i=halfSize; i<MaxElemsPerBlock; i++) {
+            nextNewChunk->values[i - halfSize] = chunkToInsert->values[i];
+        }
+        chunkToInsert->numUsed = halfSize;
+        nextNewChunk->numUsed = MaxElemsPerBlock - halfSize;
+        
+        nextNewChunk->next = chunkToInsert->next;
+        chunkToInsert->next = nextNewChunk;
+        
+        if (nextNewChunk->values[0] > newValue) {
+            insertToChunk(newValue, nextNewChunk);
+        } else {
+            insertToChunk(newValue, chunkToInsert);
+        }
+    } else {
+        // must find right position in current chunk and shift values
+        int insertPosition;
+        for (insertPosition = 0; insertPosition < chunkToInsert->numUsed; insertPosition++) {
+            if (chunkToInsert->values[insertPosition] < newValue) {
+                break;
+            }
+        }
+        // shift values
+        for (int i = chunkToInsert->numUsed-1; i >= insertPosition; i--) {
+            chunkToInsert->values[i+1] = chunkToInsert->values[i];
+        }
+        chunkToInsert->values[insertPosition] = newValue;
+        chunkToInsert->numUsed++;
+    }
 }
 
--- Assignment6/Assignment6/pqueue.h
+++ Assignment6/Assignment6/pqueue.h
@@ -132,8 +132,15 @@ class PQueue
 
   	/* This is the representation for the unsorted vector.
   	 * You will need to update this as you change representations. */
+    struct Chunk {
+        int *values;
+        int numUsed;
+        Chunk *next;
+    };
+    Chunk *head;
     
-
+    Chunk *newChunk();
+    void insertToChunk(int newValue, Chunk *&chunkToInsert);
 };
 
 
