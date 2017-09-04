94f0a9111f163f53843c9bf6123e81a32bc4ddba
antonc27 <antonc27@mail.ru>
2017-09-04 22:35:02 +0200

    - SectionHandout 9: Problem 4: Lexicon class, initilisation

--- SectionHandout9/SectionHandout9/Problem_4_WordLadderGraph.cpp
+++ SectionHandout9/SectionHandout9/Problem_4_WordLadderGraph.cpp
@@ -7,7 +7,34 @@
 //
 
 #include "Problem_4_WordLadderGraph.h"
+#include "graph_utils.h"
+#include "lexicon.h"
+
+void PrintWord(string word, int &count) {
+    count++;
+    cout << word << endl;
+}
 
 int Problem_4_WordLadderGraph_main() {
+    Lexicon lex;
+    lex.add("cake");
+    lex.add("came");
+    lex.add("camp");
+    lex.add("damp");
+    lex.add("dame");
+    lex.add("dime");
+    lex.add("dike");
+    lex.add("like");
+    lex.add("lake");
+    lex.add("lame");
+    lex.add("lamp");
+    lex.add("limp");
+    lex.add("lime");
+    
+    int count = 0;
+    lex.mapAll(PrintWord, count);
+    
+    cout << count << endl;
+    
     return 0;
 }
