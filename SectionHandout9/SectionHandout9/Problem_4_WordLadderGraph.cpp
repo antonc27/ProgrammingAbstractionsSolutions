b90f6cd65dd001f73d71a374d51125814e1a2ce9
antonc27 <antonc27@mail.ru>
2017-09-04 23:28:02 +0200

    - SectionHandout 9: Problem 4: FindShortestLaddder, WIP

--- SectionHandout9/SectionHandout9/Problem_4_WordLadderGraph.cpp
+++ SectionHandout9/SectionHandout9/Problem_4_WordLadderGraph.cpp
@@ -9,10 +9,93 @@
 #include "Problem_4_WordLadderGraph.h"
 #include "graph_utils.h"
 #include "lexicon.h"
+#include "queue.h"
+#include "stack.h"
 
-void PrintWord(string word, int &count) {
-    count++;
-    cout << word << endl;
+bool HasOneDifferentChar(string one, string two) {
+    if (one.length() != two.length()) {
+        return false;
+    }
+    int diffs = 0;
+    for (int i = 0; i < one.length(); i++) {
+        if (one[i] != two[i]) {
+            diffs++;
+        }
+    }
+    return (diffs == 1);
+}
+
+void AddWordToGraph(string word, graphT &graph) {
+    nodeT* node = findNode(graph, word);
+    if (node == NULL) {
+        node = new nodeT();
+        node->name = word;
+        graph.allNodes.add(node);
+    } else {
+        return;
+    }
+    
+    Set<nodeT *>::Iterator nodeItr = graph.allNodes.iterator();
+    while (nodeItr.hasNext()) {
+        nodeT* nextNode = nodeItr.next();
+        if (HasOneDifferentChar(node->name, nextNode->name)) {
+            AddArc(graph, node->name, nextNode->name, 1);
+            AddArc(graph, nextNode->name, node->name, 1);
+        }
+    }
+}
+
+void ConstructGraph(graphT &graph, Lexicon &lex) {
+    lex.mapAll(AddWordToGraph, graph);
+}
+
+Stack<nodeT *> FindShortestLaddder(graphT &graph, nodeT start, nodeT end) {
+    Stack<nodeT *> shortest;
+    
+    nodeT *startP = &start;
+    nodeT *endP = &end;
+    
+    Set<nodeT *> visited;
+//    visited.add(startP);
+    
+    Queue<nodeT *> queue;
+    queue.enqueue(startP);
+    
+    while (!queue.isEmpty()) {
+        nodeT *nextNode = queue.dequeue();
+        
+        if (!visited.contains(nextNode)) {
+            visited.add(nextNode);
+            shortest.push(nextNode);
+            
+            if (nextNode == endP) {
+                break;
+            }
+            
+            Set<arcT *> connected = nextNode->connected;
+            Set<arcT *>::Iterator arcItr = connected.iterator();
+            while (arcItr.hasNext()) {
+                arcT *arc = arcItr.next();
+                nodeT *nextNextNode = arc->end;
+                if (!visited.contains(nextNextNode)) {
+//                    visited.add(nextNextNode);
+                    queue.enqueue(nextNextNode);
+                }
+            }
+        }
+    }
+    
+    return shortest;
+}
+
+void PrintLadder(Stack<nodeT *> ladder) {
+    while (!ladder.isEmpty()) {
+        cout << ladder.pop()->name;
+        if (ladder.size() > 0) {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
 
 int Problem_4_WordLadderGraph_main() {
@@ -31,10 +114,15 @@ int Problem_4_WordLadderGraph_main() {
     lex.add("limp");
     lex.add("lime");
     
-    int count = 0;
-    lex.mapAll(PrintWord, count);
+    graphT wordGraph;
+    ConstructGraph(wordGraph, lex);
     
-    cout << count << endl;
+    nodeT *start = findNode(wordGraph, "came");
+    nodeT *end = findNode(wordGraph, "lime");
+    
+    Stack<nodeT *> shortest = FindShortestLaddder(wordGraph, *start, *end);
+    cout << "Shortest ladder: ";
+    PrintLadder(shortest);
     
     return 0;
 }
